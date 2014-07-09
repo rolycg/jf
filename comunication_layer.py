9a9cacd88e9716f342ed15da7f6918670e57d0e9
Roly <rolycg89@gmail.com>
2014-07-09 14:51:36 -0400

    Version 2.1.1.3 (Starting communication, first test)

--- comunication_layer.py
+++ comunication_layer.py
@@ -8,20 +8,21 @@ password = 'bla2'
 
 def broadcast():
     msg = b'Send your username'
-    dest = ('255.255.255.255', 10100)
+    dest = ('255.255.255.255', 10101)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     ssl_socket = ssl.wrap_socket(s)
     ssl_socket.sendto(msg, dest)
+    ssl_socket.settimeout(10)
     while 1:
         try:
             (buf, address) = ssl_socket.recvfrom(10104)
             if not buf:
                 break
             if buf == b'I coming':
-                checking_client(ssl_socket, address, 10101)
+                checking_client(ssl_socket, address)
                 break
-        except ssl_socket.timeout:
+        except ssl.socket_error:
             break
     start_broadcast_server()
 
@@ -42,28 +43,28 @@ def start_broadcast_server(port=10101):
                     break
                 if message == b'Send your username':
                     ssl_socket.sendto(b'I coming', address)
-                    checking_server(ssl_socket, address, 10101)
+                    checking_server(ssl_socket, address)
         except:
             ssl_socket.close()
             continue
 
 
-def checking_client(sock, address, port):
-    sock.bind((address, port))
-    sock.send((username, hashl.sha1(password).hexdigest()))
+def checking_client(sock, address):
+    print(str(address))
+    sock.sendto(username.encode() + '!!__'.encode() + str(hashl.sha1(password.encode()).hexdigest()).encode(), address)
+    print('i send ' + str(username))
     answer = sock.recv(100)
     if answer == b'OK':
-        switch_data(sock, address, port)
+        switch_data(sock, address)
 
 
-def checking_server(sock, address, port):
-    sock.bind((address, port))
-    user, passw = sock.recv(1000)
+def checking_server(sock, address):
+    user, passw = sock.recvfrom(1000)
     if user == username and hashl.sha1(password).hexdigest() == passw:
-        sock.send(b'OK')
-        switch_data(sock, address, port)
-    sock.send(b'NOT')
+        sock.sendto(b'OK', address)
+        switch_data(sock, address)
+    sock.sendto(b'NOT', address)
 
 
-def switch_data(sock, address, port):
-    pass
+def switch_data(sock, address):
+    print('i am in switch data')
